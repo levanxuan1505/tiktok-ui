d60503b4d2f6a548384710c166f4eb6794559469
Snsofm1505 <levanxuan1505@gmail.com>
2022-06-10 21:37:13 +0700

    Enter

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ function App() {
                     {publicRoutes.map((route, index) => {
                         const Page = route.component;
                         let Layout = DefaultLayout;
+
                         if (route.layout) {
                             Layout = route.layout;
                         } else if (route.layout === null) {
                             Layout = Fragment;
                         }
+
                         return (
                             <Route
                                 key={index}
