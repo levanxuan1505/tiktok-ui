a42cdbed4af3ead0828fe700ad3b38746a237ec6
Snsofm1505 <levanxuan1505@gmail.com>
2022-06-10 20:47:04 +0700

    Enter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from '~/pages/Home';
-import Following from '~/pages/Following';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
     return (
         <Router>
             <div className="App">
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/following" element={<Following />} />
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
                 </Routes>
             </div>
         </Router>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,17 @@
+import { HeaderOnly } from '~/components/Layout';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
 // router có thể dùng free ko cần đăng nhập
-const publicRoutes = [];
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
 // router chỉ được dùng khi đăng nhập, mang tính bảo mật cao, khi onClick thì lái đến trang đăng nhập
 const privateRoutes = [];
 export { publicRoutes, privateRoutes };
